041da01d930e3bbe8bd56b0d6035bdbe0fded799
Nathan Osman <nathan@quickmediasolutions.com>
2016-02-09 21:30:09 -0800

    Initial commit.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'nitroshare'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://launchpad.net/nitroshare/0.3/0.3.1/+download/nitroshare-0.3.1-windows-x86.exe'
+$url64       = 'https://launchpad.net/nitroshare/0.3/0.3.1/+download/nitroshare-0.3.1-windows-x86_64.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE_MSI_OR_MSU'
+  url            = $url
+  url64bit       = $url64
+
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+
+  softwareName   = 'nitroshare*'
+  checksum       = 'c7812bdb6a34a0246bd12b9f17b51b6b'
+  checksumType   = 'md5'
+  checksum64     = '7c26afd95d6c3562d9aa7110379c18e5'
+  checksumType64 = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
