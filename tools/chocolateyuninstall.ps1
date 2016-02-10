18f7d590187becfcfd5ba2dbdeb00c71672b5eaa
Nathan Osman <nathan@quickmediasolutions.com>
2016-02-09 22:07:09 -0800

    Minor formatting adjustments.

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName   = 'nitroshare'
-$softwareName  = 'nitroshare*'
-$installerType = 'EXE'
+$packageName     = 'nitroshare'
+$softwareName    = 'nitroshare*'
+$installerType   = 'EXE'
 
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0)
+$silentArgs      = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes  = @(0)
 
-$uninstalled = $false
-$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$uninstalled     = $false
+$local_key       = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key     = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
 $machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
 
 $key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
